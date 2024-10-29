8d6de1727fb966a57823e0933b5b52e1fd1d70ce
Red-Yu <chris830321@gmail.com>
2024-10-30 00:10:21 +0800

    調整footer動畫參數

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function updateImagePositions() {
   let offset = (window.innerWidth - 520 * 2) / 2; // 計算中間距離
-  let distance = -100; // 設定保持的距離
+  let distance = -400; // 設定保持的距離
 
   $(".imgLeft").css("transform", `translateX(${offset + distance}px)`);
   $(".imgRight").css("transform", `translateX(${(offset + distance) * -1}px)`);
@@ -15,9 +15,15 @@ $(function () {
 
     // console.log(scrollTop);
 
-    let triggerDistance = 4850;
-    // 設定移動距離
-    let moveDistance = 300;
+    let documentHeight = $(document).height();
+    let windowHeight = $(window).height();
+    // let windowWidth = $(window).width();
+
+    let triggerDistance = documentHeight - windowHeight - 150;
+    let moveDistance = 540;
+    // let moveDistance = windowWidth * 0.425;
+    let originalOffset = 270;
+    // let originalOffset = windowWidth * 0.2;
 
     if (scrollTop > triggerDistance) {
       // 更新圖像位置
@@ -25,8 +31,10 @@ $(function () {
       $(".imgRight").css("transform", "translateX(" + moveDistance + "px)");
     } else {
       // 回到原位
-      $(".imgLeft").css("transform", "translateX(0)");
-      $(".imgRight").css("transform", "translateX(0)");
+      // $(".imgLeft").css("transform", "translateX(0)");
+      // $(".imgRight").css("transform", "translateX(0)");
+      $(".imgLeft").css("transform", "translateX(-" + originalOffset + "px)");
+      $(".imgRight").css("transform", "translateX(" + originalOffset + "px)");
     }
   });
 });
