0ccec9ca57177480867bf7f4cd7a7d8beceff03e
Red-Yu <chris830321@gmail.com>
2024-10-31 23:15:07 +0800

    index動畫全數完成

---
+++ js/index.js
@@ -0,0 +1,57 @@
+function doFirst() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(".img2", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".mainImg", // 當這個元素進入視口時觸發
+      start: "top top", // 當容器的上邊緣進入視口的上邊緣
+      end: "bottom top", // 滾動到容器的底部時
+      scrub: true, // 滾動時進行動畫
+      //   markers: true,
+    },
+  });
+
+  gsap.to(".img1", {
+    scale: 1.3,
+    x: "-15%", // 最終移動到左側
+    y: "31%", // 最終移動到下方
+    scrollTrigger: {
+      trigger: ".mainImg", // 當這個元素進入視口時觸發
+      start: "top top", // 當容器的上邊緣進入視口的上邊緣
+      end: "bottom top", // 滾動到容器的底部時
+      scrub: true, // 滾動時進行動畫
+      //   markers: true,
+    },
+  });
+
+  // 獲取所有的視頻元素
+  let videos = document.querySelectorAll("video");
+
+  // 遍歷所有視頻，為每個視頻添加事件監聽器
+  videos.forEach((video) => {
+    let parent = video.parentElement;
+
+    parent.addEventListener("mouseover", () => {
+      video.play(); // 滑鼠懸停時播放視頻
+    });
+
+    parent.addEventListener("mouseout", () => {
+      video.pause(); // 滑鼠移開時暫停視頻
+      video.currentTime = 0; // 可以選擇重置到視頻的開始
+    });
+  });
+
+  // 獲取 goToTop 元素
+  const goToTopButton = document.querySelector(".goToTop");
+
+  // 添加點擊事件監聽器
+  goToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0, // 滾動到頁面頂部
+      behavior: "smooth", // 平滑滾動
+    });
+  });
+}
+
+window.addEventListener("load", doFirst);
