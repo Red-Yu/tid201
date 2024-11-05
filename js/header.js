55b17452ce8733a256a51f92f7adba18c5890c8c
Red-Yu <chris830321@gmail.com>
2024-11-06 00:27:42 +0800

    漢堡選單

--- js/header.js
+++ js/header.js
@@ -2,6 +2,19 @@ function doFirst() {
   $(document).ready(function () {
     $(".hamburger").click(function () {
       $(this).toggleClass("active");
+      $(".hamburgerCircle").toggleClass("active");
+      $(".hamburgerContent").toggleClass("active");
+
+      // 切換 .hamburgerCircle 的動畫狀態
+      if ($(".hamburger").hasClass("active")) {
+        // 添加 .active 類別，觸發進場動畫
+        $(".hamburgerCircle").removeClass("not-active").addClass("active");
+        $(".hamburgerContent").removeClass("not-active").addClass("active");
+      } else {
+        // 移除 .active 類別，觸發退場動畫
+        $(".hamburgerCircle").removeClass("active").addClass("not-active");
+        $(".hamburgerContent").removeClass("active").addClass("not-active");
+      }
     });
   });
 }
