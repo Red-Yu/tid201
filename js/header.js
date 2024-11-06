50852e7df085e5d27e58b89a5871d2f83cdecc84
Red-Yu <chris830321@gmail.com>
2024-11-06 20:34:55 +0800

    調整漢堡動畫

--- js/header.js
+++ js/header.js
@@ -8,11 +8,11 @@ function doFirst() {
       // 切換 .hamburgerCircle 的動畫狀態
       if ($(".hamburger").hasClass("active")) {
         // 添加 .active 類別，觸發進場動畫
-        $(".hamburgerCircle").removeClass("not-active").addClass("active");
+        // $(".hamburgerCircle").removeClass("not-active").addClass("active");
         $(".hamburgerContent").removeClass("not-active").addClass("active");
       } else {
         // 移除 .active 類別，觸發退場動畫
-        $(".hamburgerCircle").removeClass("active").addClass("not-active");
+        // $(".hamburgerCircle").removeClass("active").addClass("not-active");
         $(".hamburgerContent").removeClass("active").addClass("not-active");
       }
     });
